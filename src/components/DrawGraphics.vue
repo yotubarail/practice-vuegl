8a6c0049d21e4948eb3ff7c84145e3b3c1251727
yotubarail <shota_t@i.softbank.jp>
2022-03-17 00:08:27 +0900

    三角錐を描画

---
+++ src/components/DrawGraphics.vue
@@ -0,0 +1,52 @@
+<template>
+  <vgl-renderer>
+    <vgl-defs>
+      <template #tetrahedron>
+        <vgl-tetrahedron-geometry />
+      </template>
+    </vgl-defs>
+    <template #scene>
+      <vgl-scene>
+        <vgl-mesh>
+          <template #geometry>
+            <vgl-use href="tetrahedron" />
+          </template>
+          <template #material>
+            <vgl-mesh-standard-material/>
+          </template>
+        </vgl-mesh>
+        <vgl-directional-light
+          :position-x="2"
+          :position-y="1.5"
+          :position-z="1"
+        />
+      </vgl-scene>
+    </template>
+    <template #camera>
+      <vgl-perspective-camera
+        :position-x="5"
+        :position-y="1.5"
+        :position-z="1.5"
+        rotation="lookAt"
+      />
+    </template>
+  </vgl-renderer>
+</template>
+
+<script>
+import { VglRenderer, VglDefs, VglUse, VglScene, VglTetrahedronGeometry, VglMeshStandardMaterial, VglDirectionalLight, VglMesh, VglPerspectiveCamera, } from 'vue-gl';
+
+export default {
+  components: {
+    VglRenderer,
+    VglDefs,
+    VglUse,
+    VglScene,
+    VglTetrahedronGeometry,
+    VglMeshStandardMaterial,
+    VglDirectionalLight,
+    VglMesh,
+    VglPerspectiveCamera,
+  },
+};
+</script>
